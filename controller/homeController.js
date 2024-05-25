56549b90fba15fd4a13e227472beb71e1df279e6
Aryan Maheshwari <aryanmaheshwari.502@gmail.com>
2024-05-25 13:53:42 +0530

    cart ui changed

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,9 +1,18 @@
-module.exports.getHome = (req, res, next) => {
-    res.render('home', {
-        email : req.user.email,
-        isAdmin : req.user.isAdmin,
-        cartCnt : req.user.cart.products.length
-    });
+const Product = require("../model/product");
+
+module.exports.getHome = async (req, res, next) => {
+    try {
+        let products = await Product.find({}).limit(9);
+        console.log(products);
+        res.render('home', {
+            email: req.user.email,
+            isAdmin: req.user.isAdmin,
+            cartCnt: req.user.cart.products.length,
+            products
+        });
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports.getWomenSale = (req, res, next) => {
