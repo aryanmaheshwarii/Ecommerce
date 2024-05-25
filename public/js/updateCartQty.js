f1408421be200720811f2a3ccc0c642bf28b9b87
Eashan Bhatia <bhatiaeashan13@gmail.com>
2024-05-25 20:24:43 +0530

    Cart quantity issue fixed

--- public/js/updateCartQty.js
+++ public/js/updateCartQty.js
@@ -13,44 +13,50 @@ cartItem.addEventListener('click', (ev) => {
         // update the qty...
         axios.get(`/shop/increaseQty?productId=${productId}`)
             .then((res) => {
-                // console.log('req.data', res.data);
                 qtyNumber.innerText = Number(qtyNumber.innerText) + 1;
-                // console.log('price', totalPrice);
                 price.innerText = `Total Price : $ ${res.data.totalPrice}`;
-                // console.log('price', totalPrice);
-                // console.log('qty increased ', productId);
             })
             .catch((err) => {
                 console.log(err);
             })
     }
     else if (sign === '➖') {
-        console.log(qtyNumber);
         // decrease the qty...
-        axios.get(`/shop/decreaseQty?productId=${productId}`)
-            .then((res) => {
-                // console.log(res);
-                qtyNumber.innerText = Number(qtyNumber.innerText) - 1;
-                price.innerText = `Total Price : $ ${res.data.totalPrice}`;
-                // console.log('qty increased ', productId);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        if (Number(qtyNumber.innerText) > 1) {
+            axios.get(`/shop/decreaseQty?productId=${productId}`)
+                .then((res) => {
+                    qtyNumber.innerText = Number(qtyNumber.innerText) - 1;
+                    price.innerText = `Total Price : $ ${res.data.totalPrice}`;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        } else {
+            // If quantity is already 1 or less, delete the item
+            axios.get(`/shop/delete?productId=${productId}`)
+                .then((res) => {
+                    ev.target.parentElement.parentElement.parentElement.parentElement.remove();
+                    let cartCnt = document.querySelector('.cartCnt');
+                    cartCnt.innerText = Number(cartCnt.innerText) - 1;
+                    price.innerText = `$ ${res.data.totalPrice}`;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
     }
     else if (ev.target.classList.contains('deleteBtn')) {
-        console.log('pressed delete btn');
+        // delete the item...
         axios.get(`/shop/delete?productId=${productId}`)
             .then((res) => {
-                console.log(ev.target.parentElement.parentElement.parentElement.parentElement);
                 ev.target.parentElement.parentElement.parentElement.parentElement.remove();
                 let cartCnt = document.querySelector('.cartCnt');
                 cartCnt.innerText = Number(cartCnt.innerText) - 1;
                 price.innerText = `$ ${res.data.totalPrice}`;
-                console.log(res);
             })
             .catch((err) => {
                 console.log(err);
             })
     }
+
 })
